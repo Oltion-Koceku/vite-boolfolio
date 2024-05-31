bc04c6ad7e868dceacefcab25f5cd96738ded58a
Oltion-Koceku <koceku17@gmail.com>
2024-05-31 16:02:37 +0200

    milstone 4

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import './assets/main.css'
+import './assets/scss/main.scss'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
 
 import { createApp } from 'vue'
 import App from './App.vue'
